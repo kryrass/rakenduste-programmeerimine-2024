3d87ba8881fa1fb07c7ed441acb73cc10a24b601
Kryslin Rass <kryslinr@tlu.ee>
2024-10-06 17:24:12 +0300

    feat: add JWT

--- 2024-09-23/rakendus/backend/controllers/todos.controller.js
+++ 2024-09-23/rakendus/backend/controllers/todos.controller.js
@@ -1,4 +1,7 @@
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'd8b2afacc092d7ec995a67e49a4a5e9bf02bad21732c3aebc22c6f0ca9eddad6e61ced776fd4d1618f19ee04d8ef51d7bed65bff8dcf1eba68fb6bb44c5231bf';
 
 let todos = [
   { id: "5", name: "Pese nõud", priority: 2, createdAt: Date.now(), updatedAt: null, deleted: false },
@@ -49,9 +52,6 @@ exports.update = (req, res) => {
   return res.status(200).json({ message: "Todo updated successfully", todo });
 };
 
-
-
-
 exports.delete = (req, res) => {
   const { id } = req.params;
   const todo = todos.find(todo => todo.id === id); 
@@ -60,4 +60,32 @@ exports.delete = (req, res) => {
 
   todo.deleted = true;
   res.sendStatus(204);
+};
+
+// JWT tokeni asjad siit
+exports.generateToken = (req, res) => {
+  const { name } = req.query;
+
+  if (!name) {
+    return res.status(400).json({ type: "Error", message: "Peab sisaldama nime" });
+  }
+
+  const token = jwt.sign({ name }, SECRET_KEY, { expiresIn: '1h' });
+  return res.json({ token });
+};
+
+exports.verifyToken = (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(400).json({ type: "Error", message: "Peab sisaldama tokeni" });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ type: "Error", message: "Vale token" });
+    }
+
+    return res.json({ message: "Token on korrektne", decoded });
+  });
 };
--- 2024-09-23/rakendus/backend/routes/todos.routes.js
+++ 2024-09-23/rakendus/backend/routes/todos.routes.js
@@ -10,7 +10,9 @@ router.use(todosRouteMiddleware);
 
 router.get("/", todosGetRouteMiddleware, todosController.read);
 router.post("/", todosController.create);
-router.put("/", todosController.update); 
+router.put("/", todosController.update);
 router.delete("/:id", todosController.delete);
 
+router.get("/generate-token", todosController.generateToken); // GET http://localhost:8080/todos/generate-token?name=Kryslin
+router.post("/verify-token", todosController.verifyToken);
 module.exports = router;
